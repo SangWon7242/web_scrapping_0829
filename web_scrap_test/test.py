10aa47fad5cf48999ae4f4cb669868945d0bd84c
SangWon7242 <gotkdqja@gmail.com>
2024-08-30 14:17:54 +0900

    BeautifulSoup을 이용하여 html 파싱하여 데이터 추출, select, select_one 사용법

--- web_scrap_test/test.py
+++ web_scrap_test/test.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 # 요청할 URL
 url = 'https://news.naver.com/section/104'
@@ -6,8 +7,41 @@ url = 'https://news.naver.com/section/104'
 # GET 요청 보내기
 response = requests.get(url)
 
+html = """
+<nav class="menu-box-1" id="menu-box">
+  <ul>
+    <li>
+      <a class="naver" href="https://www.naver.com">네이버로 이동</a>
+    </li>
+    <li>
+      <a class="google" href="https://www.google.com">구글로 이동</a>
+    </li>
+    <li>
+      <a class="daum" href="https://www.daum.net">다음으로 이동</a>
+    </li>
+  </ul>
+</nav>
+"""
+
+# HTML 파싱
+bs = BeautifulSoup(html, 'html.parser')
+
+# select, select_one : 태그, 클래스, id로 HTML을 검색
+# bs.select('a') : HTML 상에 있는 모든 'a' 엘리먼트를 검색
+
+'''
+print(bs.select('a'))
+print(bs.select_one('.naver'))
+'''
+
+a_tags = bs.select('a')
+
+for a_tag in a_tags:
+  print(a_tag.get_text())
+
+'''
 # 응답 상태 코드 확인 (200은 성공을 의미)
 print(response.status_code)  # 200
-
 # 응답의 HTML 내용 출력
 print(response.text)
+'''
